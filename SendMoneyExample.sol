10501f1af2657fcec00e463e55903b8d84ce5295
plucena2 <43177904+plucena2@users.noreply.github.com>
2021-04-02 09:59:48 -0300

    Create SendMoneyExample.sol

---
+++ SendMoneyExample.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract SendMoneyExample {
+
+    uint public balanceReceived;
+
+    function receiveMoney() public payable {
+        balanceReceived += msg.value;
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+}
