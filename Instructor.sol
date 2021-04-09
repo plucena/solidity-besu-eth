5dd8aed22157d0b82490e5224a2dcd0ea6029899
Percival Lucena <plucena@br.ibm.com@Percivals-MacBook-Pro.local>
2021-04-09 15:25:16 -0300

    v2

---
+++ Instructor.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.18;
+
+contract Coursetro {
+    
+   string fName;
+   uint age;
+   
+   event Instructor(
+       string name,
+       uint age
+    );
+ 
+
+   function setInstructor(string _fName, uint _age) public {
+       fName = _fName;
+       age = _age;
+       Instructor(_fName, _age);        // Add this
+
+   }
+   
+   function getInstructor() public constant returns (string, uint) {
+              return (fName, age);
+   }
+    
+}
